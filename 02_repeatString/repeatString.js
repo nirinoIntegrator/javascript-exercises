118c9258564147969b9c97210e1f89aa468d1ab0
Nirino <62382348+nirinoIntegrator@users.noreply.github.com>
2024-12-31 13:58:08 +0100

    Exercise 1 and 2
    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let tmp = '';
+    if (num < 0) {
+        tmp = 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) tmp += string;
+    }
+    return tmp;
 };
 
 // Do not edit below this line
