e54f658d8f497619d9aef40104042cb0d7ef72f7
Nirino <62382348+nirinoIntegrator@users.noreply.github.com>
2024-12-31 14:31:02 +0100

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) return 'ERROR';
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+    let sum = 0;
+    let i = (x < y) ? x : y;
+    let j = (x > y) ? x : y;
+    for (; i <= j; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
